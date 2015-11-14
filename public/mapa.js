1bbc96806cc998065c8ebc5a5c7cc37ed58c211c
EdgarMagdaleno <edgar_mb5@hotmail.com>
2015-11-14 19:16:59 +0000

    Sattelite map update

--- public/mapa.js
+++ public/mapa.js
@@ -1,30 +1,28 @@
 function initialize() {
-  var myLatLng = {lat: 32.4197712, lng: -115.0876732};
   var mapProp = {
-    center:new google.maps.LatLng(32.4197712,-115.0876732),
-    zoom:6,
+    center:new google.maps.LatLng(19.3580023,-99.2579486),
+    zoom:20,
     mapTypeId:google.maps.MapTypeId.SATELLITE
   };
   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-
   var markers = [];
   lecturas = [];
   lecturas.push({
-    lat: 30,
-    lng: -115,
+    lat: 19.3580500,
+    lng: -99.2578400,
     humidity: 0
   })
 
   lecturas.push({
-    lat: 31,
-    lng: -115,
+    lat: 19.3580150,
+    lng: -99.2579420,
     humidity: 1
   })
 
   lecturas.push({
-    lat: 32,
-    lng: -115,
+    lat: 19.3579250,
+    lng: -99.2579000,
     humidity: 2
   })
 
@@ -34,7 +32,7 @@ function initialize() {
       position: {lat:Number(lectura.lat), lng:Number(lectura.lng)},
       map: map,
       icon: getPinImage(humidity),
-      title: 'Hello World!'
+      title: 'I/O HACK'
     }));
   });
 
@@ -49,7 +47,7 @@ function initialize() {
           position: {lat:Number(lectura.lat), lng:Number(lectura.lng)},
           map: map,
           icon: getPinImage(humidity),
-          title: 'Hello World!'
+          title: 'I/O HACK'
         }));
       });      
     }
